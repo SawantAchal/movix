d4c1d8c7c09da2daf2e9f25601aa342298e55b05
SawantAchal <achals1718@gmail.com>
2024-01-22 18:19:18 +0530

    create a dynamic component to change tabs for filter moview base on day or week . using index

---
+++ src/Components/SwitchTabs/SwitchTabs.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import './SwitchTabs.scss'
+
+const SwitchTabs = ({data ,onTabChange}) => {
+    const [selectedTab , setSelectedTabs] = useState(0)
+    const [left, setLeft] = useState(0)
+
+    const activeTab = (tab ,index) =>{
+        setLeft(index * 100)
+        setTimeout(() => {
+            setSelectedTabs(index)
+        }, 3000);
+        onTabChange(tab , index)
+    }
+
+  return (
+    <div className='switchingTabs'>
+        <div className="tabItems">
+            {
+                data.map((tab , index) => (
+                    <span key={index} className={`tabItem ${selectedTab === index ? "active" : ""}`} onClick={() => activeTab(tab , index)}>{tab}</span>
+                ))
+            }
+            <span className="movingBg" style={{left:left}}/>
+        </div>
+    </div>
+  )
+}
+
+export default SwitchTabs
