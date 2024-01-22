febc041d200b6eeb28266752e303c5d664a88dbe
SawantAchal <achals1718@gmail.com>
2024-01-23 01:38:14 +0530

    add detailBaner componet as pass props in that

--- src/Pages/details/Details.jsx
+++ src/Pages/details/Details.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './Details.scss'
+import { useParams } from 'react-router-dom'
+import DetailsBanner from './detailsBanner/DetailsBanner'
+import useFetch from '../../hooks/useFetch'
 
 const Details = () => {
+  const {mediaType , id} = useParams()
+  // api call for banner
+  //api for video of movie or tv shoe
+  const {data , loading } = useFetch(`/${mediaType}/${id}/videos`);
+  //api for cast in movie
+  const {data :credits , loading:creditsLoading } = useFetch(`/${mediaType}/${id}/credits`)
+
   return (
-    <div>Details</div>
+    <div>
+      {/* to show only trailer we pass [0] it means first video */}
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew}/>
+    </div>
   )
 }
 
