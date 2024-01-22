098a4496c1f1594d67e8e938cab13224444639c7
SawantAchal <achals1718@gmail.com>
2024-01-22 21:08:06 +0530

    Add all component in home page

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './Home.scss'
 import HeroBanner from './herBanner/HeroBanner'
+import Trending from './trending/Trending'
+import Popular from './popular/Popular'
+import TopRated from './topRated/TopRated'
 
 const Home = () => {
   return (
     <div>
       <HeroBanner />
+      <Trending/>
+      <Popular/>
+      <TopRated/>
     </div>
   )
 }
