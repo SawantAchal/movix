e1eed9fc58f15c49b521cbd89f13de630b6cd22f
SawantAchal <achals1718@gmail.com>
2024-01-22 21:09:22 +0530

    add function for direction of carousel in navigation method

--- src/Components/corousel/Carousal.jsx
+++ src/Components/corousel/Carousal.jsx
@@ -1,25 +1,28 @@
 import React, { useRef } from "react";
-import {
-    BsFillArrowLeftCircleFill,
-    BsFillArrowRightCircleFill,
-} from "react-icons/bs";
+import {BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill,} from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ContentWrapper from "../ContentWrapper/ContentWrapper";
 import Img from "../lazyLoadImage/Img";
 import PosterFallback from "../../assets/no-poster.png";
-// import CircleRating from "../circleRating/CircleRating";
-// import Genres from "../genres/Genres";
 import './Carousal.scss'
 import dayjs from "dayjs";
+import CirculeRating from "../circuleRating/CirculeRating";
+import Genres from "../genres/Genres";
 
-const Carousal = ({data , loading}) => {
+const Carousal = ({data , loading , endpoint}) => {
     const carouselContainer = useRef();
     const {url} = useSelector((state) => state.home)
     const navigate = useNavigate()
 
     const navigation = (direction) => {
-
+        const container = carouselContainer.current;
+        // to move Carousal
+        const scrollAmount = direction === 'left' ? container.scrollLeft - (container.offsetWidth + 20) : container.scrollLeft + (container.offsetWidth + 20);
+        container.scrollTo({
+            left: scrollAmount,
+            behavior:'smooth',
+        })
     }
 
     // created method for do not repeat the class for sKItem
@@ -43,15 +46,17 @@ const Carousal = ({data , loading}) => {
             <BsFillArrowRightCircleFill className="carouselRightNav arrow" onClick={() => navigation('right')}/>
             {
                 !loading ? (
-                    <div className="carouselItems">
+                    <div className="carouselItems" ref={carouselContainer}>
                         {
                             data?.map((item) => {
                                 //img url
                                 const posterurl = item.poster_path ? url.poster + item.poster_path : PosterFallback
                                 return(
-                                    <div key={item.id} className="carouselItem" >
+                                    <div key={item.id} className="carouselItem" onClick={()=>navigate(`/${item.media_type || endpoint}/${item.id}`)}>
                                         <div className="posterBlock">
                                             <Img src={posterurl} />
+                                            <CirculeRating rating={item.vote_average.toFixed(1)}/>
+                                            <Genres data={item.genre_ids.slice(0,2)}/>
                                         </div>
                                         <div className="textBlock">
                                             <span className="title">{item.title || item.name}</span>
